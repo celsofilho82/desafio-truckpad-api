e63388de13e6ed1bfa3400b35f27009bf17076d2
Celso Ribeiro <celsofilho82@gmail.com>
2020-03-29 17:05:09 -0300

    setting in model trip accepts nested attributes for location

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -3,4 +3,18 @@ class Trip < ApplicationRecord
   belongs_to :truck, optional: true
   belongs_to :origin, class_name: 'Location'
   belongs_to :destination, class_name: 'Location'
+  accepts_nested_attributes_for :origin
+  accepts_nested_attributes_for :destination
+
+  def as_json(options={})
+    super(
+      except: [:created_at, :updated_at, :origin_id, :destination_id],
+      include: {
+        origin: { except: [:created_at, :updated_at] },
+        destination: { except: [:created_at, :updated_at] }
+      }
+    )
+    
+  end
+
 end
