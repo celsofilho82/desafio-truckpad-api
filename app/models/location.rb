645df680f2d1ee5af0bd58a3d80761ffd139ba0d
Celso Ribeiro <celsofilho82@gmail.com>
2020-03-28 23:00:43 -0300

    create method full_city_state for geocoder

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,4 +1,11 @@
 class Location < ApplicationRecord
   has_many :origin_trips, class_name: 'Trip', foreign_key: 'origin_id'
   has_many :destination_trips, class_name: 'Trip', foreign_key: 'destination_id'
+  geocoded_by :full_city_state
+  after_validation :geocode
+
+  def full_city_state
+    [city, state].compact.join(', ')
+  end
+
 end
